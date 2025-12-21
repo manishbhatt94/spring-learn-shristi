cb83346c78a37c71adb68412f1e308888929ed0d
Manish Bhatt <manishbhatt594@gmail.com>
2025-12-21 12:10:22 +0530

    Add example for diff types of auto-wiring

--- SpringBasics/src/main/java/com/spring/autowiring/IShape.java
+++ SpringBasics/src/main/java/com/spring/autowiring/IShape.java
@@ -1,5 +1,7 @@
 package com.spring.autowiring;
 
+// Should we add @Component annotation here or not
+// @Component
 public interface IShape {
 
 	void calcArea(int x, int y);
--- SpringBasics/src/main/java/com/spring/autowiring/ShapeFactory.java
+++ SpringBasics/src/main/java/com/spring/autowiring/ShapeFactory.java
@@ -7,12 +7,40 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShapeFactory {
 
+	// auto-wiring by type
 	@Autowired
 	@Qualifier("rectangle")
-	private IShape shape;
+	private IShape shape; // = new Rectangle
+
+	// auto-wiring by name - instance variable name & bean name are same
+	@Autowired
+	private IShape triangle;
+
+	// auto-wiring by constructor
+	private IShape myShape;
+
+	// auto-wiring by constructor
+	// - need to use @Qualifier(<name of implementation class bean>)
+	// since there are multiple instance fields of IShape type
+	public ShapeFactory(@Qualifier("parallelogram") IShape myShape) {
+		super();
+		this.myShape = myShape;
+	}
+
+	// auto-wiring using setter
+	private IShape anotherShape;
+
+	// auto-wiring using setter
+	@Autowired
+	public void setAnotherShape(@Qualifier("triangle") IShape anotherShape) {
+		this.anotherShape = anotherShape;
+	}
 
 	void printArea(int x, int y) {
 		shape.calcArea(x, y);
+		triangle.calcArea(x, y);
+		myShape.calcArea(x, y);
+		anotherShape.calcArea(x, y);
 	}
 
 }
