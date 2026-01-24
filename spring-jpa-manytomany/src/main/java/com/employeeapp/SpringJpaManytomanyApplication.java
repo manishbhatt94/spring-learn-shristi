ee2b65aaf1318e6b3be7a205a168c4a0b722d0bc
Manish Bhatt <manishbhatt594@gmail.com>
2026-01-24 15:11:07 +0530

    Add AppClient to test the application

--- spring-jpa-manytomany/src/main/java/com/employeeapp/SpringJpaManytomanyApplication.java
+++ spring-jpa-manytomany/src/main/java/com/employeeapp/SpringJpaManytomanyApplication.java
@@ -1,64 +1,33 @@
 package com.employeeapp;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.employeeapp.model.Category;
-import com.employeeapp.model.CourseDto;
-import com.employeeapp.model.CourseLevel;
-import com.employeeapp.model.EmployeeDto;
-import com.employeeapp.model.Mode;
-import com.employeeapp.service.ICourseService;
-import com.employeeapp.service.IEmployeeService;
+import com.employeeapp.bootstrap.DataInitializer;
+import com.employeeapp.client.AppClient;
 
 @SpringBootApplication
 public class SpringJpaManytomanyApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJpaManytomanyApplication.class, args);
-	}
+	@Autowired
+	private DataInitializer dataInitializer;
 
 	@Autowired
-	private ICourseService courseService;
-	private IEmployeeService employeeService;
+	private AppClient appClient;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringJpaManytomanyApplication.class, args);
+
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		// add courses to database
-		CourseDto courseDto1 = new CourseDto(null, "Core Java for Beginners", Mode.ONLINE, Category.BE.getCategory(),
-				CourseLevel.BEGINNER, "Sripriya Mathan", null);
-		courseService.addCourse(courseDto1);
-
-		CourseDto courseDto2 = new CourseDto(null, "Gen AI - A bird's eye view", Mode.ONLINE, Category.ML.getCategory(),
-				CourseLevel.BEGINNER, "Gaurav Sen", null);
-		courseService.addCourse(courseDto2);
-
-		CourseDto courseDto3 = new CourseDto(null, "Architecting React Apps", Mode.OFFLINE, Category.FE.getCategory(),
-				CourseLevel.INTERMEDIATE, "Dan Abramov", null);
-		courseService.addCourse(courseDto3);
-
-		// for working with employee
-
-		// get 3 courseDtos by id
-		CourseDto courseDto11 = courseService.getById(10);
-		CourseDto courseDto22 = courseService.getById(11);
-		CourseDto courseDto33 = courseService.getById(12);
-		// create a list
-		List<CourseDto> coursesList = Arrays.asList(courseDto11, courseDto22, courseDto33);
-		System.out.println(coursesList);
-		// add to a set
-		Set<CourseDto> courses = new HashSet<>(coursesList);
-		// create employeeDtos and add course to employee
-		EmployeeDto employee = new EmployeeDto(null, "Rakesh Roshan", "Tech", "Pune", courses);
-		employeeService.addEmployee(employee);
+		dataInitializer.initData();
+		appClient.run();
 
 	}
 
---
+++ spring-jpa-manytomany/src/main/java/com/employeeapp/client/AppClient.java
@@ -0,0 +1,102 @@
+package com.employeeapp.client;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.employeeapp.model.Category;
+import com.employeeapp.model.CourseDto;
+import com.employeeapp.model.CourseLevel;
+import com.employeeapp.model.EmployeeDto;
+import com.employeeapp.model.Mode;
+import com.employeeapp.service.ICourseService;
+import com.employeeapp.service.IEmployeeService;
+
+@Component
+public class AppClient {
+
+	@Autowired
+	private ICourseService courseService;
+
+	@Autowired
+	private IEmployeeService employeeService;
+
+	public void run() {
+
+		addCoursesAndEmployees();
+		// getAll();
+
+	}
+
+	private void addCoursesAndEmployees() {
+
+		System.out.println("\n------ addCoursesAndEmployees --------\n");
+
+		// add courses to database
+		CourseDto courseDto1 = courseService.addCourse(new CourseDto(null, "Core Java for Beginners", Mode.ONLINE,
+				Category.BE.getCategory(), CourseLevel.BEGINNER, "Sripriya Mathan", null));
+
+		CourseDto courseDto2 = courseService.addCourse(new CourseDto(null, "Gen AI - A bird's eye view", Mode.ONLINE,
+				Category.ML.getCategory(), CourseLevel.BEGINNER, "Gaurav Sen", null));
+
+		CourseDto courseDto3 = courseService.addCourse(new CourseDto(null, "Architecting React Apps", Mode.OFFLINE,
+				Category.FE.getCategory(), CourseLevel.INTERMEDIATE, "Dan Abramov", null));
+
+		// for working with employee
+
+		// create a list
+		List<CourseDto> coursesList = Arrays.asList(courseDto1, courseDto2, courseDto3);
+		System.out.println(coursesList);
+
+		// add to a set
+		Set<CourseDto> courses = new HashSet<>(coursesList);
+		// create employeeDtos and add course to employee
+		EmployeeDto employee = new EmployeeDto(null, "Rakesh Roshan", "Tech", "Pune", courses);
+		employeeService.addEmployee(employee);
+
+		System.out.println("Successfully added 3 more courses & 1 more employee - via Service methods.");
+		System.out.println();
+
+	}
+
+	private void getAll() {
+
+		System.out.println("\n------ getAll --------\n");
+		employeeService.getAll().forEach(System.out::println);
+		System.out.println();
+
+	}
+
+//	private void getById(int restaurantId) {
+//
+//		System.out.println("\n------ getById --------\n");
+//		System.out.println("> restaurantId = " + restaurantId);
+//		System.out.println(restaurantService.getById(restaurantId));
+//		System.out.println();
+//
+//	}
+//
+//	private void getByCity(String city) {
+//
+//		System.out.println("\n------ getByCity --------\n");
+//		System.out.println("> city = " + city);
+//		restaurantService.getByCity(city).forEach(System.out::println);
+//		System.out.println();
+//
+//	}
+//
+//	private void getByItemTypeCuisine(ItemType itemType, Cuisine cuisine) {
+//
+//		System.out.println("\n------ getByItemTypeCuisine --------\n");
+//		System.out.println("> itemType = " + itemType.name());
+//		System.out.println("> cuisine = " + cuisine.getCuisineType());
+//		restaurantService.getByItemTypeCuisine(itemType.name(), cuisine.getCuisineType()).forEach(System.out::println);
+//		System.out.println();
+//
+//	}
+
+}
