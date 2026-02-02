abaf90585b03744f68321710f2dd51cf0509e031
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-02 14:54:46 +0530

    Add starter files for bookapp serving on REST endpoints (No persistence)

---
+++ spring-restapi-bookapp-basic-no-db/src/main/java/com/bookapp/SpringRestapiBookappBasicNoDbApplication.java
@@ -0,0 +1,13 @@
+package com.bookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiBookappBasicNoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiBookappBasicNoDbApplication.class, args);
+	}
+
+}
