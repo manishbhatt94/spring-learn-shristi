66d35abf922bd7681e4a9b7e98c688540e4f05be
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-16 14:42:20 +0530

    Add dummy spring security project to demo default user login logout
    flow setup by spring security module

---
+++ spring-restapi-securitybasics/src/main/java/com/productapp/SpringRestapiSecuritybasicsApplication.java
@@ -0,0 +1,13 @@
+package com.productapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiSecuritybasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiSecuritybasicsApplication.class, args);
+	}
+
+}
