315bd4e3be66fde537060e10ee17cc8a22e479bb
Manish Bhatt <manishbhatt594@gmail.com>
2026-02-17 17:28:03 +0530

    Add Spring Initializr boilerplate for app to demo JWT based auth

---
+++ spring-productapp-jwtsecurity/src/main/java/com/productapp/SpringProductappJwtsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.productapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductappJwtsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductappJwtsecurityApplication.class, args);
+	}
+
+}
